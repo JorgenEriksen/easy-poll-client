75112172de52366009e576c44a837f19e80c3102
Jorgen Eriksen <jrgn93@gmail.com>
2022-08-24 17:17:30 +0200

    testing @emotion/css

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,41 @@
-import './index.css';
-import { TextField } from '@mui/material';
+import "./index.css";
+import { css, cx } from '@emotion/css'
+import { TextField, Button } from "@mui/material";
+
+const classes = {
+  landingContainer: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#d4f7ff",
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: 'column',
+    paddingTop: '200px',
+  },
+  landingEnterButton : {
+    width: '100%',
+    marginTop: '20px',
+  },
+  landingFrontbox: {
+    backgroundColor: "white",
+    padding: "20px",
+    width: "400px",
+  },
+};
 
 const Landing = () => {
-
-    return (<div className="landing-container">
-            <div><TextField label="InviteToken">asd</TextField></div>
-        </div>);
-}
+  return (
+    <div className={css(classes.landingContainer)}>
+      <h1 className={css(classes.landingHeader)}>EasyPoll</h1>
+      <div className={css(classes.landingFrontbox)}>
+        <TextField label="InviteToken" style={{ width: "100%" }} />
+        <Button variant="contained" style={classes.landingEnterButton}>
+          Enter
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
