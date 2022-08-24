86ccfb7c375b4482da54ad78e8c2f1d11650ffd8
Jorgen Eriksen <jrgn93@gmail.com>
2022-08-24 15:39:13 +0200

    init

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import { TextField } from '@mui/material';
+
+const Landing = () => {
+
+    return (<div className="landing-container">
+            <div><TextField label="InviteToken">asd</TextField></div>
+        </div>);
+}
+
+export default Landing;
